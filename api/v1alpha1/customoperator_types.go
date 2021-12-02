450687799501435d1dbdcdeb481a1909f1867d0e
Ashima Garg <Ashima.Ashima1@ibm.com>
2021-12-02 20:15:31 +0530

    step3: define the api

--- api/v1alpha1/customoperator_types.go
+++ api/v1alpha1/customoperator_types.go
@@ -29,7 +29,8 @@ type CustomOperatorSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of CustomOperator. Edit customoperator_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Foo string `json:"foo,omitempty"`
+	Replicas int32 `json:"replicas"`
 }
 
 // CustomOperatorStatus defines the observed state of CustomOperator
